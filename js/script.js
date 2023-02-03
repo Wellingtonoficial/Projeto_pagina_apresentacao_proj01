e458694b371d843e9b78a23cd7ad47c8f350b8e4
Wellington <wellingtonoficial00@gmail.com>
2023-02-02 21:48:59 -0300

    .

--- js/script.js
+++ js/script.js
@@ -7,7 +7,7 @@ btnDrop01.addEventListener('click', ()=>{
     document.querySelector('.subMenuDrop01').classList.toggle('active')
 
     const img = document.querySelector('.arrowDrop01')
-    document.querySelector('.subMenuDrop01').classList.contains('active')?img.src = "../imgs/icon-arrow-down.svg":img.src = "../imgs/icon-arrow-up.svg"
+    document.querySelector('.subMenuDrop01').classList.contains('active')?img.src = "./imgs/icon-arrow-down.svg":img.src = "./imgs/icon-arrow-up.svg"
 })
 
 
@@ -18,7 +18,7 @@ btnDrop02.addEventListener('click', ()=>{
     document.querySelector('.subMenuDrop02').classList.toggle('active')
 
     const img = document.querySelector('.arrowDrop02')
-    document.querySelector('.subMenuDrop02').classList.contains('active')?img.src = "../imgs/icon-arrow-down.svg":img.src = "../imgs/icon-arrow-up.svg"
+    document.querySelector('.subMenuDrop02').classList.contains('active')?img.src = "./imgs/icon-arrow-down.svg":img.src = "./imgs/icon-arrow-up.svg"
 })
 
 
@@ -29,7 +29,7 @@ btnMenu.addEventListener('click', ()=>{
     document.querySelector('.menu').classList.toggle('ativado')
     
     const imgmenu = document.querySelector('.imgMenu')
-    document.querySelector('.menu').classList.contains('ativado')?imgmenu.src = "../imgs/icon-close-menu.svg":imgmenu.src = "../imgs/icon-menu.svg"
+    document.querySelector('.menu').classList.contains('ativado')?imgmenu.src = "./imgs/icon-close-menu.svg":imgmenu.src = "./imgs/icon-menu.svg"
 
     // mudar cor do body
     let bodyBackground =  document.querySelector('.desfoque')
